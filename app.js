ab3490a46da2a902f256d239ab7774e69dd8e329
Pedro Lucas <90810508+PedroLucaas@users.noreply.github.com>
2024-09-05 19:16:49 +0100

    final

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const express = require("express");
-var bodyParser = require('body-parser')
-
+var bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 const mysql = require('mysql2');
+const nodemailer = require("nodemailer");
+const multer = require('multer');
+
+const upload = multer();
 dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(bodyParser.json())
 
-const db = mysql.createConnection({
-    host: 'localhost', // Seu host
-    user: 'root',      // Seu usuário do MySQL
-    password: 'root', // Sua senha do MySQL
-    database: 'teste' // Seu banco de dados
+const transporter = nodemailer.createTransport({
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "teste@heloplantier.com",
+    pass: process.env.PASS,
+  },
+});
+
+app.post("/", upload.none(), (req, res) => {
+  var { name, email } = req.body;
+  const db = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DATABASE
   });
+
+  
+  transporter.sendMail(
+      {
+        from: "teste@heloplantier.com",
+        to: email,
+        replyTo: "teste@heloplantier.com",
+        subject: "OBRIGADO",
+        text: `
+          its working bro?`
+      },
+      (err, info) => {
+        console.log(info);
+        console.log(err);
+      },
+    );
   
   db.connect((err) => {
     if (err) {
@@ -25,23 +59,43 @@ const db = mysql.createConnection({
     console.log('Conectado ao banco de dados MySQL');
   });
 
+  const sql = 'INSERT INTO user SET name=?, email=?';
 
-app.post("/", (req, res) => {
-    var { name, email, phone } = req.body;
-
-    const sql = 'INSERT INTO emails SET name=?, email=?, phone=?';
-
-    db.query(sql, [name, email, phone], (err, result) => {
-      if (err) {
-        console.error('Erro ao adicionar usuário:', err);
-        res.status(500).send('Erro ao adicionar usuário');
-        return;
-      }
-  
-    });
+  db.query(sql, [name, email], (err) => {
+    if (err) {
+      console.error('Erro ao adicionar usuário:', err);
+      res.status(500).send('Erro ao adicionar usuário');
+      return;
+    }
+  });
     res.send('Usuário adicionado com sucesso');
 });
 
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server Listening at http://localhost:${process.env.PORT}`);
+    console.log(`Server Listening at http://localhost:${process.env.PORT} + + `);
 });
+
+function runQuery (con, sqlQuery) {
+    return new Promise((resolve, reject) => {
+        console.log("START");
+        if(con){
+            con.connect(function (err) {
+                if (err) throw err;
+            });
+
+            if (sqlQuery) {
+                con.query(sqlQuery, function (error, result, fields) {
+                    // con.end(); // end connection
+                    if (error) {
+                        throw error;
+                    } else {
+                        return resolve(result);
+                    }
+                });
+            }
+        }
+
+    });
+
+}
